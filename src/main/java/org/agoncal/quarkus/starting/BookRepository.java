1e8895dcd374483de1b7dbc6347935f14ea6200d
William Ospino <wospino@live.com>
2022-06-03 16:00:12 -0500

    last commit, quarkus project

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,12 +1,10 @@
 package org.agoncal.quarkus.starting;
 
-<<<<<<< HEAD
-=======
+
 import io.smallrye.config.ConfigMapping;
 import org.eclipse.microprofile.config.inject.ConfigProperties;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
->>>>>>> 641c2f0 (first commit, quarkus project)
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -15,33 +13,21 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
-<<<<<<< HEAD
-
+    @ConfigProperty(name ="books.genre", defaultValue = "Sci-Fi")
+    String genre;
 
     public List<Book> getAllBooks() {
         return List.of(
                 new Book(1, "Understanding quarkus", "Antonio", 2020, "IT"),
                 new Book(2, "Practising quarkus", "Antonio", 2020, "IT"),
                 new Book(3, "Efective Java", "Josh Blocj", 2001, "IT"),
-                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT")
-=======
-    @ConfigProperty(name ="books.genre", defaultValue = "Sci-Fi")
-    String genre;
-
-    public List<Book> getAllBooks() {
-        return List.of(
-                new Book(1, "Understanding quarkus", "Antonio", 2020, genre),
-                new Book(2, "Practising quarkus", "Antonio", 2020, genre),
-                new Book(3, "Efective Java", "Josh Blocj", 2001, genre),
-                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, genre)
->>>>>>> 641c2f0 (first commit, quarkus project)
-        );
+                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT"));
     }
 
 
 
 
-    public Optional<Book> getBook( int id){
+    public Optional<Book> getBook(int id){
         return getAllBooks().stream().filter(book -> book.id== id).findFirst();
     }
 }
--- src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
@@ -1,9 +1,29 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
 
-    // Execute the same tests but in native mode.
+    @Test
+    public void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+                pathParam("id", 1).
+                when().get("/api/books/{id}").
+                then()
+                .statusCode(200)
+                .body("title", is("Understanding quarkus"))
+                .body("author", is("Antonio"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("IT"));
+
+    }
 }
